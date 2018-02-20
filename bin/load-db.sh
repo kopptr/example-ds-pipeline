fc84cabe26e81ea6c01adb8dcbab095f18985f7e
Tim Kopp <tkopp@pivotal.io>
2018-02-20 12:56:06 -0500

    Add script to import data to postgres

---
+++ bin/load-db.sh
@@ -0,0 +1,12 @@
+#!/usr/env bash
+
+psql $URL <<EOF
+DROP TABLE IF EXISTS ticket_prices;
+
+CREATE TABLE ticket_prices (
+	event VARCHAR,
+	division VARCHAR,
+	avg_ticket_price FLOAT
+);
+\\copy ticket_prices FROM five-thirtyeight-data/nfl-ticket-prices/2014-average-ticket-price.csv WITH (FORMAT csv, HEADER);
+EOF
